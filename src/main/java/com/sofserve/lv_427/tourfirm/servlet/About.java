12a749c0de8765f17c7bafe026ff6685288a71ea
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-07-18 17:47:14 +0300

    fix servlet about

--- src/main/java/com/sofserve/lv_427/tourfirm/servlet/About.java
+++ src/main/java/com/sofserve/lv_427/tourfirm/servlet/About.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "/about")
+@WebServlet("/about")
 public class About extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 

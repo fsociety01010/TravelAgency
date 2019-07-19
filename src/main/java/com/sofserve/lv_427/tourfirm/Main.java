20f303d1d86acc21001178fc480ae2a19b8b5934
fsociety01010 <alexbmx@mail.ua>
2019-07-19 12:57:22 +0300

    Adding CountryDao

--- src/main/java/com/sofserve/lv_427/tourfirm/Main.java
+++ src/main/java/com/sofserve/lv_427/tourfirm/Main.java
@@ -1,11 +1,20 @@
 package com.sofserve.lv_427.tourfirm;
 
 import com.sofserve.lv_427.tourfirm.dao.CityDao;
+import com.sofserve.lv_427.tourfirm.dao.CountryDao;
+import com.sofserve.lv_427.tourfirm.model.Country;
+import com.sofserve.lv_427.tourfirm.utils.JdbcConnector;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        new CityDao().findAll();
+         CountryDao countryDao =new CountryDao(JdbcConnector.getConnection());
+
+        List<Country> countryList = countryDao.findAll();
+        for ( Country el : countryList ) {
+            System.out.println(el.getCountryName());
+        }
     }
 }
